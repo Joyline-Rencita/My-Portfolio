bb7505375cd3240999e6c0935beb3d934037925f
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-02-01 13:08:05 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -60,20 +60,6 @@ async function fetchData(type = "skills") {
     return data;
 }
 
-function showSkills(skills) {
-    let skillsContainer = document.getElementById("skillsContainer");
-    let skillHTML = "";
-    skills.forEach(skill => {
-        skillHTML += `
-        <div class="bar">
-              <div class="info">
-                <img src=${skill.icon} alt="skill" />
-                <span>${skill.name}</span>
-              </div>
-            </div>`
-    });
-    skillsContainer.innerHTML = skillHTML;
-}
 
     // <!-- tilt js effect starts -->
     VanillaTilt.init(document.querySelectorAll(".tilt"), {
